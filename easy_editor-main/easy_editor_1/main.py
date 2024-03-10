6d459d04d7a7a259312451b663003383ecc00726
AndriiMali <147975355+AndriiMali@users.noreply.github.com>
2024-03-10 13:29:15 +0200

    Add files via upload

---
+++ easy_editor-main/easy_editor_1/main.py
@@ -0,0 +1,52 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog
+from ui import Ui_Dialog
+import os
+from ImageProcessor import ImageProccesor
+
+app = QApplication([])
+win = QMainWindow()
+
+ui = Ui_Dialog()
+ui.setupUi(win)
+#-----------------------------
+workdir = ""
+img_proc = ImageProccesor(ui)
+
+def chooseWorkDir():
+    global workdir
+    workdir = QFileDialog.getExistingDirectory()
+    print(workdir)
+
+
+def filter(files, extensions):
+    graphical_files = []
+
+    for file in files:
+        for ext in extensions:
+            if file.endswith(ext):
+               graphical_files.append(file)
+
+    return graphical_files
+
+def showFilenamesList():
+    chooseWorkDir()
+    extensions = [".png", ".jpg", ".jpeg", ".bmp", ".gif"]
+    
+    filenames = os.listdir(workdir)
+    filenames = filter(filenames, extensions)
+    print(filenames)
+    ui.files_list.clear()
+    ui.files_list.addItems(filenames)
+ui.dear_btn.clicked.connect(showFilenamesList)
+
+def showChossenImage():
+    if ui.files_list.currentItem():
+        filename = ui.files_list.currentItem().text()
+        img_proc.open(workdir, filename)
+        img_proc.show()
+
+
+ui.files_list.currentItemChanged.connect(showChossenImage)
+#-------------------------------
+win.show()
+app.exec()
